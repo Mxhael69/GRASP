796c633b9f1d892747a3975650209c8db8121981
Mxhael69 <derpmangoa@gmail.com>
2026-08-25 20:28:56 +0800

    Uploading

---
+++ script.js
@@ -0,0 +1,11 @@
+function showInfo(id) {
+
+    const info = document.getElementById(id);
+
+    if (info.style.display === "block") {
+        info.style.display = "none";
+    } else {
+        info.style.display = "block";
+    }
+
+}
